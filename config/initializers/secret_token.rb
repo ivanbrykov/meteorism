519d7c3dc62c6518f876637c98e0c977b219fc45
Prelang Builder <builder@prelang.com>
2014-04-02 05:54:46 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meteorism::Application.config.secret_key_base = 'd01fe49f4bdad44f9d108b833af0f351ef5ec08092e43ad31a7611661cb3522014abb159213f5f4fb602676e96bce638edab47783871398d674bd7c7474d2469'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Meteorism::Application.config.session_store :cookie_store, key: '_meteorism_session'
